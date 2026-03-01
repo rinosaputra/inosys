8fcdf606a2ffcee2fe76ce1e6aacb5db1bcfcd40
Rino Saputra <rinosaputraas@gmail.com>
2026-03-01 15:36:25 +0700

    chord: add api auth

---
+++ src/integrations/better-auth/basic/config.ts
@@ -0,0 +1,34 @@
+import type { BetterAuthOptions } from "better-auth";
+import { createPool } from "mysql2/promise";
+
+import { connection } from "#/integrations/knex";
+
+export const betterAuthBasicConfig: BetterAuthOptions = {
+  database: createPool({
+    host: connection.host,
+    port: connection.port,
+    user: connection.user,
+    password: connection.password,
+    database: connection.database,
+    timezone: "Z", // Important to ensure consistent timezone values
+  }),
+  user: {
+    modelName: "_user_lists",
+    fields: {
+      name: "fullName",
+      email: "emailAddress",
+    }
+  },
+  session: {
+    modelName: "_user_sessions",
+  },
+  account: {
+    modelName: "_user_accounts",
+  },
+  verification: {
+    modelName: "_user_verifications",
+  },
+  emailAndPassword: {
+    enabled: true,
+  }
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,10 @@
 import { betterAuth } from "better-auth";
-import { createPool } from "mysql2/promise";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 
-import { connection } from "#/integrations/knex";
+import { betterAuthBasicConfig } from "#/integrations/better-auth/basic/config";
 
 export const auth = betterAuth({
-  database: createPool({
-    host: connection.host,
-    port: connection.port,
-    user: connection.user,
-    password: connection.password,
-    database: connection.database,
-    timezone: "Z", // Important to ensure consistent timezone values
-  }),
+  ...betterAuthBasicConfig,
+
+  plugins: [tanstackStartCookies()],
 })
