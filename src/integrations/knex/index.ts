7eb5b3593a9f9ff307cd43a4f54359e06fd3165c
Rino Saputra <rinosaputraas@gmail.com>
2026-03-01 15:42:33 +0700

    chore: move connection variable

--- src/integrations/knex/index.ts
+++ src/integrations/knex/index.ts
@@ -1,20 +1,4 @@
-import z from "zod";
-
-export const connection = z
-  .object({
-    host: z.string().nonempty(),
-    port: z.number().int().positive(),
-    user: z.string().nonempty(),
-    password: z.string(),
-    database: z.string().nonempty(),
-  })
-  .parse({
-    host: import.meta.env.DB_HOST,
-    port: Number(import.meta.env.DB_PORT),
-    user: import.meta.env.DB_USER,
-    password: import.meta.env.DB_PASSWORD,
-    database: import.meta.env.DB_NAME,
-  });
+import { connection } from "#/lib/connection";
 
 const knex = require('knex')({
   client: 'mysql2',
